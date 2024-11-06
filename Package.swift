c21c2f7ed526aa7307392bd7d923c901601587a1
eonist <30n1st@gmail.com>
2024-11-06 12:34:13 +0100

    Lint

--- Package.swift
+++ Package.swift
@@ -5,17 +5,17 @@ let package = Package(
    name: "AppConfigKit",
    platforms: [
       .macOS(.v14), // macOS 14 and later
-      .iOS(.v17), // iOS 17 and later
+      .iOS(.v17) // iOS 17 and later
    ],
    products: [
       .library(
          name: "AppConfigKit",
-         targets: ["AppConfigKit"]),
+         targets: ["AppConfigKit"])
    ],
    dependencies: [
       .package(url: "https://github.com/eonist/UserDefaultSugar", branch: "master"), // UserDefaultSugar package from GitHub
       .package(url: "https://github.com/sentryco/SDUtil", branch: "main"),
-      .package(url: "https://github.com/sentryco/Key", branch: "main"),
+      .package(url: "https://github.com/sentryco/Key", branch: "main")
 //      .package(url: "https://github.com/sentryco/PersistenceKit", branch: "main"),
 //      .package(url: "https://github.com/sentryco/SecUserStore", branch: "main"),
    ],
@@ -25,6 +25,6 @@ let package = Package(
          dependencies: [ "SDUtil", "UserDefaultSugar", /*"PersistenceKit",*/ /*"SecUserStore",*/ "Key"]), // Defines the Auth target with dependencies on DatabaseLib and UserDefaultSugar
       .testTarget(
          name: "AppConfigKitTests",
-         dependencies: ["AppConfigKit"]), // Defines the AuthTests target with a dependency on Auth
+         dependencies: ["AppConfigKit"]) // Defines the AuthTests target with a dependency on Auth
    ]
 )
